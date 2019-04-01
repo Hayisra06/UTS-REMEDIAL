4e2f3310d969ad90586072a63f1d24fb5fe9c976
Hayisra <49136315+Hayisra06@users.noreply.github.com>
2019-03-31 22:44:04 -0700

    upload 1.cpp
    Program untuk menampilkan NAMA< NIM KELAS dan TAHUN angkatan.

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout<<" Nama  :   Hayisra"<<endl;
+cout<<" NIM   :   1829141008"<<endl;
+cout<<" Kelas :   TEKOM B"<<endl;
+cout<<" Tahun :   2018"<<endl;
+
+return 0;
+}
