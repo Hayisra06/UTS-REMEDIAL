3049df2bfc886b1d2da7f109b7256e02323b77ae
Hayisra <49136315+Hayisra06@users.noreply.github.com>
2019-04-01 18:43:21 -0700

    upload 2.cpp
    membuat siku2 terbalik  manggunakan perulangan "for"

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int i;
+int j;
+
+for(i=2; ii<=8; i++){
+  for (j=i+2; j<=8; j++){
+  cout<<"#"<<" ";
+  }
+  cout<<endl;
+  }
+  return 0;
+  }
