c020fc4fda79403ae6ce09433ff4caf648612562
Hayisra <49136315+Hayisra06@users.noreply.github.com>
2019-04-02 00:09:33 -0700

    update 2.cpp
    Menampilkan data berupa nama, NIM dan IPK

--- 2.cpp
+++ 2.cpp
@@ -2,14 +2,28 @@
 using namespace std;
 int main ()
 {
-int i;
-int j;
-
-for(i=2; ii<=8; i++){
-  for (j=i+2; j<=8; j++){
-  cout<<"#"<<" ";
+  struct orang
+  { 
+    char nama[10];
+    int NIM;
+    int IPK;
+  };
+  orang org[2];
+  int i;
+  for(i=0; i<2; i++)
+  {
+    cout<<"Nama : ";
+    cin>>org[i].nama;
+    cout<<"NIM : ";
+    cin>>org[i].NIM;
+    cout<<"IPK : ";
+    cin>>org[i].IPK;
+    cout<<endl;
   }
-  cout<<endl;
-  }
-  return 0;
-  }
+  for (i=0; i<2; i++)
+  {
+    cout<<"Nama : "<<org[i].nama<<endl;
+    cout<<"NIM  : "<<org[i].NIM<<endl;
+    cout<<"IPK  : "<<org[i].IPK<<endl;
+  }return 0;
+}
